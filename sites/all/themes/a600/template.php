226b44be408fae181a9c40f6a8ebd2ea62ade317
Ivan Fayzulin <kemsnake@gmail.com>
2014-08-16 13:46:16 +0700

    added project search. added styles

--- sites/all/themes/a600/template.php
+++ sites/all/themes/a600/template.php
@@ -5,3 +5,7 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * a600 theme.
  */
+
+function a600_preprocess_node(&$vars) {
+  $vars['theme_hook_suggestions'][] = 'node__' . $vars['type'] . '__' . $vars['view_mode'];
+}
