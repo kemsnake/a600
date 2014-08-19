bdaeab455667435ff52e1fbbe9814423b6d4e4b4
Ivan Fayzulin <kemsnake@gmail.com>
2014-08-20 02:55:00 +0700

    changed project view. added configs.

--- sites/all/themes/a600/template.php
+++ sites/all/themes/a600/template.php
@@ -9,3 +9,7 @@
 function a600_preprocess_node(&$vars) {
   $vars['theme_hook_suggestions'][] = 'node__' . $vars['type'] . '__' . $vars['view_mode'];
 }
+
+function a600_page_alter(&$page) {
+  drupal_set_breadcrumb(array());
+}
