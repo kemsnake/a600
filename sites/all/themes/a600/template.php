0aa2ee15f5eeac43076416232405aeb5d3eb92a4
Ivan Fayzulin <kemsnake@gmail.com>
2014-09-03 02:26:48 +0700

    added extra filters

--- sites/all/themes/a600/template.php
+++ sites/all/themes/a600/template.php
@@ -14,28 +14,10 @@ function a600_page_alter(&$page) {
   drupal_set_breadcrumb(array());
 }
 
-/* Custom fieldset - without wrapper inside fieldset */
-function a600_fieldset($variables) {
-  $element = $variables['element'];
-  element_set_attributes($element, array('id'));
-  _form_set_class($element, array('form-wrapper'));
-
-  $output = '<fieldset' . drupal_attributes($element['#attributes']) . '>';
-  if (!empty($element['#title'])) {
-    // Always wrap fieldset legends in a SPAN for CSS positioning.
-    $output .= '<legend><span class="fieldset-legend">' . $element['#title'] . '</span></legend>';
-  }
-  if (!empty($element['#description'])) {
-    $output .= '<div class="fieldset-description">' . $element['#description'] . '</div>';
-  }
-  $output .= $element['#children'];
-  if (isset($element['#value'])) {
-    $output .= $element['#value'];
-  }
-  $output .= "</fieldset>\n";
-  return $output;
-}
-
+/*
+ * Меняем JS модуля на свой. Нам нужна не цискличная карусель,
+ * а просто пэйджер из картинок(со скрываемыми кнопками).
+ */
 function a600_js_alter(&$js) {
   if (isset($js[drupal_get_path('module', 'galleryformatter') . '/theme/infiniteCarousel.js'])) {
     unset($js[drupal_get_path('module', 'galleryformatter') . '/theme/infiniteCarousel.js']);
