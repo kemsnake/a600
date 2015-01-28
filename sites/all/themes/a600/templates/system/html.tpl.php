bccdd7f09f7ebd6ab559cccdbae8925384c72649
ivan faizulin <kemsnake@gmail.com>
2015-01-29 00:52:22 +0700

    added ya.metrics, chat and webmaster control file

--- sites/all/themes/a600/templates/system/html.tpl.php
+++ sites/all/themes/a600/templates/system/html.tpl.php
@@ -58,6 +58,15 @@
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  <!--Start of Zopim Live Chat Script-->
+  <script type="text/javascript">
+    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+      d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+      _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+      $.src='//v2.zopim.com/?2YLRwq6cgbHaWdUWjHYWoFaWAAx0fBVb';z.t=+new Date;$.
+        type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+  </script>
+  <!--End of Zopim Live Chat Script-->
 </head>
 <body<?php print $attributes;?>>
   <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
